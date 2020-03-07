fa0381707481d8d743aeb8e16ac24f6e93bfad8f
gurudutt-durgadas <gurudutt.shetti@gmail.com>
2020-03-06 20:44:06 -0800

    Updates

---
+++ precache-manifest.988de39929abc578a55b95ca3de3ba77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a54d22a16babc0f2656592c9d36b3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c064d22cf14570ab2d47",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "418e82f820cd7b4c3192",
+    "url": "/static/css/main.96ba2d0e.chunk.css"
+  },
+  {
+    "revision": "c064d22cf14570ab2d47",
+    "url": "/static/js/2.527ed06b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.527ed06b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418e82f820cd7b4c3192",
+    "url": "/static/js/main.a666349c.chunk.js"
+  },
+  {
+    "revision": "a79fe040d527b0f16196",
+    "url": "/static/js/runtime-main.a19a8bef.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
